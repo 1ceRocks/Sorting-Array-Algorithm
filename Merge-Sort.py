97935e7f36fa6935b3c3a5de6b378efb8546a049
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-01-21 13:55:12 +0800

    creating program mechanics for merge(left, right)

--- Merge-Sort.py
+++ Merge-Sort.py
@@ -9,4 +9,21 @@ def merge_sort(arrayVal):
     left = merge_sort(arrayVal[:mid])
     right = merge_sort(arrayVal[:mid])
     
-    return merge(left, right) #! merge def function to be created later after program commit.
+    return merge(left, right)
+
+def merge(left, right):
+    output = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            output.append(left[i])
+            i += 1
+        else:
+            output.append(right[j])
+            j += 1
+            
+    output.extend(left[i:])
+    output.extend(right[j:])
+    
+    return output
