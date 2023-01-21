aa08b171bf467a64ca096a3b2de50fe617eb4981
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-01-21 13:41:36 +0800

    implementing main() def for printing output

--- Bubble-Sort.py
+++ Bubble-Sort.py
@@ -4,8 +4,19 @@ def bubble_sort(arrayVal):
     for i in range(n-1):
         for j in range(n-1):
             if arrayVal[j] > arrayVal[j+1]:
-                tmp = list[j]
+                tmp = arrayVal[j]
                 arrayVal[j] = arrayVal[j+1]
                 arrayVal[j+1] = tmp
                 
     return arrayVal
+
+def main():
+    arrayVal = [52, 95, 18, 78, 12, 60, 34, 6, 83, 32]
+    bubble_sort(arrayVal)
+    print(arrayVal)
+    
+    arrayVal = [3, 9, 2, 1]
+    bubble_sort(arrayVal)
+    print(arrayVal)
+    
+main()
