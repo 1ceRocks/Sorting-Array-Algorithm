16bcf27b26e4dc92ac92dfa9f2e32cc7f0eabe95
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-01-21 14:14:05 +0800

    building main() function for program output
    also fixed some code naming and parentheses used for calling function that would prevent any TypeError in our future program code.

--- Quick-Sort.py
+++ Quick-Sort.py
@@ -1,18 +1,29 @@
 def partition(arrayVal, low, high):
     i = low
-    pivot = arrayVal(high)
+    pivot = arrayVal[high]
     
     for j in range(low, high):
-        if list[j] <= pivot:
-            list[i], list[j] = list[j], list[i]
+        if arrayVal[j] <= pivot:
+            arrayVal[i], arrayVal[j] = arrayVal[j], arrayVal[i]
             i += 1
             
-    list[i], list[high] = list[high], list[i]
+    arrayVal[i], arrayVal[high] = arrayVal[high], arrayVal[i]
     
     return i
 
-def quick_sort(list, low, high):
+def quick_sort(arrayVal, low, high):
     if low < high:
-        partition_index = partition(list, low, high)
-        quick_sort(list, low, partition_index - 1)
-        quick_sort(list, low, partition_index + 1, high)
+        partition_index = partition(arrayVal, low, high)
+        quick_sort(arrayVal, low, partition_index - 1)
+        quick_sort(arrayVal, partition_index + 1, high)
+        
+def main():
+    arrayVal = [52, 95, 18, 78, 12, 60, 34, 6, 83, 32]
+    quick_sort(arrayVal, 0, len(arrayVal) - 1)
+    print(arrayVal)
+    
+    arrayVal = [3, 9, 2, 1]
+    quick_sort(arrayVal, 0, len(arrayVal) - 1)
+    print(arrayVal)
+    
+main()
